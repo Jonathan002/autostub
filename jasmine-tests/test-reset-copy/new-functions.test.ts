ac53e132a3a210bd4fe7abdcfc032aa61e9205ff
Jonathan002 <JonathanF002@gmail.com>
2019-09-05 15:56:23 -0700

    reverse copy - 1

---
+++ jasmine-tests/test-reset-copy/new-functions.test.ts
@@ -0,0 +1,7 @@
+export function hmm(thing: string, bur: number): number {
+  return 
+}
+// Test Bad Space
+export       function      test  (args: number, space: string) {
+  return 'functionfunctionfunction';
+};
